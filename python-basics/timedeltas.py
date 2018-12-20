52933377cc1163c585f88b5bd5309c90417ac714
Colin Smith <cmsgoboston41@gmail.com>
2018-12-20 15:46:25 -0500

    Add second part of basics review

--- python-basics/timedeltas.py
+++ python-basics/timedeltas.py
@@ -22,15 +22,22 @@ print("One year from now it will be: " + str(now +timedelta(days=365)))
 print("In 2 days and 3 weeks, it will be " + str(now + timedelta(days=2, weeks=3)))
 
 # calculate the date 1 week ago, formatted as a string
-
+t = datetime.now() - timedelta(weeks=1)
+s = t.strftime("%A %B %d, %Y")
+print("One week ago it was: " + s)
 
 ### How many days until April Fools' Day?
+today = date.today()
+afd = date(today.year, 4, 1)
 
 
 # use date comparison to see if April Fool's has already gone for this year
 # if it has, use the replace() function to get the date for next year
-
+if afd < today:
+    print("April Fool's Day already went by %d days ago" % ((today-afd).days))
+    afd = afd.replace(year = today.year+1)
 
 # Now calculate the amount of time until April Fool's Day  
-
+time_to_afd = afd - today
+print("It's just", time_to_afd.days, "days until April Fool's Day")
 
